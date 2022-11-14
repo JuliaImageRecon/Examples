e101c5a9e7366a3e9b5ab6d1e07e7a6c405eb706
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-11-14 10:53:11 -0500

    Refine CS-SENSE example

--- docs/lit/mri/4-cs-sense-2d.jl
+++ docs/lit/mri/4-cs-sense-2d.jl
@@ -35,22 +35,24 @@ except
 =#
 
 # Packages used in this demo (run `Pkg.add` as needed):
-using ImagePhantoms: ellipse_parameters, SheppLoganBrainWeb, Ellipse, phantom
+using ImagePhantoms: ellipse_parameters, SheppLoganBrainWeb, ellipse, phantom
 using ImagePhantoms: mri_smap_fit, mri_spectra
 #using ImageFiltering: imfilter, centered
 using ImageMorphology: dilate #, label_components # imfill
 using LazyGrids: ndgrid
 using ImageGeoms: embed, embed!
-using MIRT: Afft, Aodwt
+using MIRT: Aodwt # todo Asense
 using MIRTjim: jim, prompt
 using MIRT: ir_mri_sensemap_sim
 using MIRT: pogm_restart
 using LinearAlgebra: norm
+using LinearMapsAA: LinearMapAA
 using Plots; default(markerstrokecolor=:auto, label="")
-using FFTW: fft, fftshift
+using FFTW: fft!, ifft!, fftshift!
 using Random: seed!
 using InteractiveUtils: versioninfo
 
+if !@isdefined(ydata) || false
 
 # The following line is helpful when running this jl-file as a script;
 # this way it will prompt user to hit a key after each image is displayed.
@@ -72,14 +74,15 @@ y = (-(ny÷2):(ny÷2-1)) * dy
 # Modified Shepp-Logan phantom with random complex phases
 object = ellipse_parameters(SheppLoganBrainWeb() ; disjoint=true, fovs)
 seed!(0)
-object[:,end] = [1; randn(ComplexF32, 9)] # random phases
-object = Ellipse(object)
+object = vcat( (object[1][1:end-1]..., 1), # random phases
+    [(ob[1:end-1]..., randn(ComplexF32)) for ob in object[2:end]]...)
+object = ellipse(object)
 oversample = 3
 Xtrue = phantom(x, y, object, oversample)
-#Xtrue = reverse(Xtrue, dims=2)
 cfun = z -> cat(dims = ndims(z)+1, real(z), imag(z))
 #clim = (0,9)
-jim(x, y, cfun(Xtrue), "True image\n real | imag"; ncol=1)
+jim(:aspect_ratio, :equal)
+jim(x, y, cfun(Xtrue), "True image\n real | imag"; ncol=2)
 
 
 # Mask (support for image reconstruction)
@@ -92,21 +95,22 @@ jim(x, y, mask + abs.(Xtrue), "Mask + |Xtrue|")
 
 # Make sensitivity maps, normalized so SSoS = 1:
 ncoil = 4
-smap = ir_mri_sensemap_sim(dims=(nx,ny), ncoil=ncoil, orbit_start=[45])
-p1 = jim(x, y, smap, "Sensitivity maps raw");
+smap = ir_mri_sensemap_sim(dims=(nx,ny); ncoil, orbit_start=[45])
+jif(args...; kwargs...) = jim(args...; prompt=false, kwargs...)
+p1 = jif(x, y, smap, "Sensitivity maps raw");
 
-ssos = sqrt.(sum(abs.(smap).^2, dims=ndims(smap))) # SSoS
+ssos = sqrt.(sum(abs2, smap; dims=ndims(smap))) # SSoS
 ssos = selectdim(ssos, ndims(smap), 1)
-p2 = jim(x, y, ssos, "SSoS for ncoil=$ncoil");
+p2 = jif(x, y, ssos, "SSoS for ncoil=$ncoil");
 
-for ic=1:ncoil
+for ic in 1:ncoil
     selectdim(smap, ndims(smap), ic) ./= ssos
 end
 smap .*= mask
-ssos = sqrt.(sum(abs.(smap).^2, dims=ndims(smap))) # SSoS
-stacker = x -> [(@view x[:,:,i]) for i=1:size(x,3)]
+ssos = sqrt.(sum(abs2, smap; dims=ndims(smap))) # SSoS
+stacker = x -> [(@view x[:,:,i]) for i in 1:size(x,3)]
 smaps = stacker(smap)
-p3 = jim(x, y, smaps, "Sensitivity maps normalized");
+p3 = jif(x, y, smaps, "Sensitivity maps normalized")
 jim(p1, p2, p3)
 
 
@@ -116,7 +120,7 @@ fy = (-(ny÷2):(ny÷2-1)) / (ny*dy)
 gx, gy = ndgrid(fx, fy);
 
 # Somewhat random 1D phase-encode sampling:
-seed!(0); sampfrac = 0.4; samp = rand(ny÷2) .< sampfrac; sig = 1
+seed!(0); sampfrac = 0.4; samp = rand(ny÷2) .< sampfrac
 tmp = rand(ny÷2) .< 0.5; samp = [samp .* tmp; reverse(samp .* .!tmp)] # symmetry
 samp .|= (abs.(fy*dy) .< 1/8) # fully sampled center ±1/8 phase-encodes
 ny_count = count(samp)
@@ -133,11 +137,11 @@ jim(fx, fy, samp, title="k-space sampling ($ny_count / $ny = $samp_frac%)")
 
 kmax = 7
 fit = mri_smap_fit(smaps, embed; mask, kmax, deltas=(dx,dy))
-jimf(args...; kwargs...) = jim(args...; prompt=false, kwargs...)
 jim(
- jimf(x, y, cfun(smaps), "Original maps"; clim=(-1,1), nrow=4),
- jimf(x, y, cfun(fit.smaps), "Fit maps"; clim=(-1,1), nrow=4),
- jimf(x, y, cfun(100 * (fit.smaps - smaps)), "error * 100"; nrow=4),
+ jif(x, y, cfun(smaps), "Original maps"; clim=(-1,1), nrow=4),
+ jif(x, y, cfun(fit.smaps), "Fit maps"; clim=(-1,1), nrow=4),
+ jif(x, y, cfun(100 * (fit.smaps - smaps)), "error * 100"; nrow=4),
+ layout = (1,3),
 )
 
 # Analytical spectra computation for complex phantom using all smaps
@@ -146,17 +150,20 @@ ytrue = mri_spectra(gx[samp], gy[samp], object, fit)
 ytrue = hcat(ytrue...)
 
 # Noisy under-sampled k-space data:
+sig = 1
+sig = 0 # todo test
 ydata = ytrue + sig * √(2) * randn(ComplexF32, size(ytrue)) # complex noise!
 ydata = ComplexF32.(ydata) # save memory
 ysnr = 20 * log10(norm(ytrue) / norm(ydata - ytrue)) # data SNR in dB
 
 # Display zero-filled data:
-logger = (x; min=-6) -> log10.(max.(abs.(x) / maximum(abs, x), (10.)^min))
+logger = (x; min=-6, up=maximum(abs,x)) -> log10.(max.(abs.(x) / up, (10.)^min))
 jim(:abswarn, false) # suppress warnings about showing magnitude
 jim(fx, fy, logger(embed(ytrue[:,1],samp)),
     title="k-space |data| (zero-filled, coil 1)",
     xlabel="νx", ylabel="νy")
 
+end # ydata
 
 #=
 ### Prepare to reconstruct
@@ -178,18 +185,19 @@ function Asense(samp, smaps)
     work2 = Array{ComplexF32}(undef, dims)
     ncoil = length(smaps)
     function forw!(y,x)
-        for ic = 1:ncoil
-            work1 .= x .* smaps[ic]
-            fft!(work2, work1)
-            y[:,ic] .= work2[samp]
+        for ic in 1:ncoil
+            @. work1 = x * smaps[ic]
+            fftshift!(work1, fft!(fftshift!(work2, work1)))
+            y[:,ic] .= work1[samp]
         end
     end
     function back!(x,y)
-        for ic = 1:ncoil
-            embed!(work1, y[:,ic])
-            ifft!(work2, work1)
-            copyto!(work1, smaps[ic])
-            conj!(work1)
+        for ic in 1:ncoil
+            embed!(work1, (@view y[:,ic]), samp)
+#           ifft!(work2, work1)
+            fftshift!(work1, ifft!(fftshift!(work2, work1)))
+            copyto!(work2, smaps[ic])
+            conj!(work2)
             if ic == 1
                 @. x = work1 * work2 * N
             else
@@ -197,18 +205,30 @@ function Asense(samp, smaps)
             end
         end
     end
-    A = LinearMapAO(forw!, back!, (ncoil*count(samp), N);
-        odim = (ncoil,count(samp)), idim=dims, T=ComplexF32)
+    A = LinearMapAA(forw!, back!, (ncoil*count(samp), N);
+        odim = (count(samp),ncoil), idim=dims, T=ComplexF32)
     return A
 end
 
-F = Afft(samp) # operator!
-throw(0)
+A = Asense(samp, smaps) # operator!
+
+y0 = embed(ytrue, samp)
+y1 = embed(A * Xtrue, samp)
+jim(
+ jif(logger(y0; up=maximum(abs,y0))),
+ jif(logger(y1; up=maximum(abs,y0))),
+ jif(logger(y1 - y0; up=maximum(abs,y0))),
+)
+
+#src # test adjoint
+#src xt = randn(ComplexF32, A._idim)
+#src yt = randn(ComplexF32, A._odim)
+#src vec(yt)' * vec(A * xt) ≈ conj(vec(xt)' * vec(A' * yt))
 
 # Initial image based on zero-filled reconstruction:
 nrmse = (x) -> round(norm(x - Xtrue) / norm(Xtrue) * 100, digits=1)
-X0 = 1.0f0/(nx*ny) * (F' * y)
-jim(X0, "|X0|: initial image; NRMSE $(nrmse(X0))%")
+X0 = 1.0f0/(nx*ny) * (A' * ydata)
+jim(x, y, X0, "|X0|: initial image; NRMSE $(nrmse(X0))%")
 
 
 #=
@@ -236,21 +256,21 @@ not the approximation coefficients.
 =#
 
 # Orthogonal discrete wavelet transform operator (`LinearMapAO`):
-W, scales, _ = Aodwt((nx,ny) ; T = eltype(F))
+W, scales, _ = Aodwt((nx,ny) ; T = eltype(A))
 isdetail = scales .> 0
 jim(
-    jim(scales, "wavelet scales"),
-    jim(real(W * Xtrue) .* isdetail, "wavelet detail coefficients"),
+ jif(scales, "wavelet scales"),
+ jif(real(W * Xtrue) .* isdetail, "wavelet detail coefficients"),
 )
 
 
 # Inputs needed for proximal gradient methods:
-Az = F * W' # another operator!
+Az = A * W' # another operator!
 Fnullz = (z) -> 0 # cost function in `z` not needed
-f_gradz = (z) -> Az' * (Az * z - y)
+f_gradz = (z) -> Az' * (Az * z - ydata)
 f_Lz = nx*ny # Lipschitz constant for single coil Cartesian DFT
 regz = 0.03 * nx * ny # oracle from Xtrue wavelet coefficients!
-costz = (z) -> 1/2 * norm(Az * z - y)^2 + regz * norm(z,1) # 1-norm regularizer
+costz = (z) -> 1/2 * norm(Az * z - ydata)^2 + regz * norm(z,1) # 1-norm regularizer
 soft = (z,c) -> sign(z) * max(abs(z) - c, 0) # soft thresholding
 g_prox = (z,c) -> soft.(z, isdetail .* (regz * c)) # proximal operator (shrink details only)
 z0 = W * X0
@@ -277,25 +297,28 @@ function fun_fista(iter, xk, yk_z, is_restart)
 end;
 
 # Run and compare three proximal gradient methods:
-niter = 50
-z_ista, out_ista = pogm_restart(z0, Fnullz, f_gradz, f_Lz; mom=:pgm, niter=niter,
-    restart=:none, restart_cutoff=0., g_prox=g_prox, fun=fun_ista)
+niter = 20
+z_ista, out_ista = pogm_restart(z0, Fnullz, f_gradz, f_Lz;
+    mom=:pgm, niter,
+    restart=:none, restart_cutoff=0., g_prox, fun=fun_ista)
 Xista = W'*z_ista
 @show nrmse(Xista)
 
-z_fista, out_fista = pogm_restart(z0, Fnullz, f_gradz, f_Lz; mom=:fpgm, niter=niter,
-    restart=:gr, restart_cutoff=0., g_prox=g_prox, fun=fun_fista)
+z_fista, out_fista = pogm_restart(z0, Fnullz, f_gradz, f_Lz;
+    mom=:fpgm, niter,
+    restart=:gr, restart_cutoff=0., g_prox, fun=fun_fista)
 Xfista = W'*z_fista
 @show nrmse(Xfista)
 
-z_pogm, out_pogm = pogm_restart(z0, Fnullz, f_gradz, f_Lz; mom=:pogm, niter=niter,
-    restart=:gr, restart_cutoff=0., g_prox=g_prox, fun=fun_fista)
+z_pogm, out_pogm = pogm_restart(z0, Fnullz, f_gradz, f_Lz;
+    mom=:pogm, niter,
+    restart=:gr, restart_cutoff=0., g_prox, fun=fun_fista)
 Xpogm = W'*z_pogm
 @show nrmse(Xpogm)
 
 jim(
-    jim(Xfista, "FISTA/FPGM"),
-    jim(Xpogm, "POGM with ODWT"),
+ jif(x, y, Xfista, "FISTA/FPGM"),
+ jif(x, y, Xpogm, "POGM with ODWT"),
 )
 #src savefig("xpogm_odwt.pdf")
 
@@ -303,11 +326,11 @@ jim(
 # ## POGM is fastest
 
 # Plot cost function vs iteration:
-cost_ista = [out_ista[k][1] for k=1:niter+1]
-cost_fista = [out_fista[k][1] for k=1:niter+1]
-cost_pogm = [out_pogm[k][1] for k=1:niter+1]
+cost_ista = [out_ista[k][1] for k in 1:niter+1]
+cost_fista = [out_fista[k][1] for k in 1:niter+1]
+cost_pogm = [out_pogm[k][1] for k in 1:niter+1]
 cost_min = min(minimum(cost_ista), minimum(cost_pogm))
-plot(xlabel="iteration k", ylabel="Relative cost")
+pc = plot(xlabel="iteration k", ylabel="Relative cost")
 scatter!(0:niter, cost_ista  .- cost_min, label="Cost ISTA")
 scatter!(0:niter, cost_fista .- cost_min, markershape=:square, label="Cost FISTA")
 scatter!(0:niter, cost_pogm  .- cost_min, markershape=:utriangle, label="Cost POGM")
@@ -318,22 +341,25 @@ isinteractive() && prompt();
 
 
 # Plot NRMSE vs iteration:
-nrmse_ista = [out_ista[k][2] for k=1:niter+1]
-nrmse_fista = [out_fista[k][2] for k=1:niter+1]
-nrmse_pogm = [out_pogm[k][2] for k=1:niter+1]
-plot(xlabel="iteration k", ylabel="NRMSE %", ylims=(3,6.5))
+nrmse_ista = [out_ista[k][2] for k in 1:niter+1]
+nrmse_fista = [out_fista[k][2] for k in 1:niter+1]
+nrmse_pogm = [out_pogm[k][2] for k in 1:niter+1]
+pn = plot(xlabel="iteration k", ylabel="NRMSE %")#, ylims=(3,6.5))
 scatter!(0:niter, nrmse_ista, label="NRMSE ISTA")
 scatter!(0:niter, nrmse_fista, markershape=:square, label="NRMSE FISTA")
 scatter!(0:niter, nrmse_pogm, markershape=:utriangle, label="NRMSE POGM")
 #src savefig("nrmse_pogm_odwt.pdf")
 
+#
+isinteractive() && prompt();
+
 # Show error images:
-p1 = jim(Xtrue, "true")
-p2 = jim(X0, "X0: initial")
-p3 = jim(Xpogm, "POGM recon")
-p5 = jim(X0 - Xtrue, "X0 error", clim=(0,2))
-p6 = jim(Xpogm - Xtrue, "Xpogm error", clim=(0,2))
-jim(p2, p3, p5, p6)
+p1 = jif(x, y, Xtrue, "true")
+p2 = jif(x, y, X0, "X0: initial"; xlabel = "$(nrmse(X0))%")
+p3 = jif(x, y, Xpogm, "POGM recon"; xlabel = "$(nrmse(Xpogm))%")
+p5 = jif(x, y, X0 - Xtrue, "X0 error", clim=(0,2))
+p6 = jif(x, y, Xpogm - Xtrue, "Xpogm error", clim=(0,2))
+pe = jim(p2, p3, p5, p6)
 
 
 
