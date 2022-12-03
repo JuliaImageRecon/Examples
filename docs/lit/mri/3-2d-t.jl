b4d13cf3e8fc6b6b61983074a077a4149df4d848
Jeff Fessler <fessler@umich.edu>
2022-12-02 22:53:16 -0500

    Refine 2d+t example

--- docs/lit/mri/3-2d-t.jl
+++ docs/lit/mri/3-2d-t.jl
@@ -1,5 +1,5 @@
 #---------------------------------------------------------
-# # [2D dynamic](@id 3-2d-t)
+# # [2D dynamic: temporal TV](@id 3-2d-t)
 #---------------------------------------------------------
 
 #src 2019-06-13, Jeff Fessler
@@ -29,7 +29,7 @@ This entire page was generated using a single Julia file:
 #md # [`3-2d-t.ipynb`](@__BINDER_ROOT_URL__/mri/3-2d-t.ipynb).
 
 using ImageGeoms: ImageGeom
-using ImagePhantoms: shepp_logan, SouthPark, phantom, ellipse
+using ImagePhantoms: shepp_logan, SouthPark, phantom, ellipse, spectrum
 using MIRTjim: jim, prompt
 using MIRT: Anufft, diffl_map, ncg
 using MIRT: ir_mri_sensemap_sim, ir_mri_kspace_ga_radial
@@ -47,9 +47,7 @@ jim(:abswarn, false); # suppress warnings about display of |complex| images
 isinteractive() && jim(:prompt, true);
 
 
-# ## Create (synthetic) data
-
-# Generate dynamic image sequence:
+# ## Define dynamic image sequence
 N = (60,64)
 fov = 220
 nt = 8 # frames
@@ -58,16 +56,17 @@ ig = ImageGeom(; dims = N, deltas=(fov,fov) ./ N)
 object0 = shepp_logan(SouthPark(); fovs = (fov,fov))
 objects = Array{typeof(object0)}(undef, nt)
 xtrue = Array{ComplexF32}(undef, N..., nt)
-for it=1:nt
+for it in 1:nt
     tmp = copy(object0)
-    width2 = 15 + 5 * sin(2*pi*it/nt) # mouth open/close
+    width2 = 15 + 5 * sin(2π*it/nt) # mouth open/close
     mouth = tmp[2]
     tmp[2] = ellipse(mouth.center, (mouth.width[1], width2), mouth.angle[1], mouth.value)
     objects[it] = tmp
     xtrue[:,:,it] = phantom(axes(ig)..., tmp, 4)
 end
-jimxy = (args...; kwargs...) -> jim(axes(ig)..., args...; kwargs...)
-jimxy(xtrue, "True images")
+jimxy = (args...; aspect_ratio=1, kwargs...) ->
+    jim(axes(ig)..., args...; aspect_ratio, kwargs...)
+jimxy(xtrue, "True images"; nrow=2)
 
 # Animate true image:
 anim1 = @animate for it in 1:nt
@@ -82,16 +81,16 @@ plot(1:nt, abs.(xtrue[ix,iy,:]), label="ix=$ix, iy=$iy",
     marker=:o, xlabel="frame")
 
 #
-isinteractive() && jim(:prompt, true);
+isinteractive() && prompt();
 
 
-# Generate k-space sampling and data:
+# ## Define k-space sampling
 accelerate = 3
 nspf = round(Int, maximum(N)/accelerate) # spokes per frame
 
 Nro = maximum(N)
 Nspoke = nspf * nt
-kspace = ir_mri_kspace_ga_radial(Nro = Nro, Nspoke = Nspoke)
+kspace = ir_mri_kspace_ga_radial(; Nro, Nspoke)
 fovs = (fov, fov)
 kspace[:,:,1] ./= fovs[1]
 kspace[:,:,2] ./= fovs[2]
@@ -101,16 +100,15 @@ kspace = reshape(kspace, Nro, nspf, nt, 2)
 
 # Plot sampling (in units of cycles/pixel):
 ps = Array{Any}(undef, nt)
-for it=1:nt
+for it in 1:nt
     ps[it] = scatter(kspace[:,:,it,1] * fovs[1], kspace[:,:,it,2] * fovs[2],
-        xtick=(-1:1)*0.5, ytick=(-1:1)*0.5, xlim=(-1,1).*0.52, ylim=(-1,1).*0.52,
-        aspect_ratio=1, markersize=1, title="Frame $it")
-#src plot(ps[it]); gui()
+        xtick=(-1:1)*0.5, ytick=(-1:1)*0.5, xlim=(-1,1).*0.5, ylim=(-1,1).*0.5,
+        aspect_ratio=1, markersize=1, title="Frame $it", widen=true)
 end
-plot(ps..., layout=(2,4))
+plot(ps...; layout=(2,4))
 
 #
-isinteractive() && jim(:prompt, true);
+isinteractive() && prompt();
 
 anim2 = @animate for it in 1:nt
     plot(ps[it])
@@ -120,66 +118,77 @@ gif(anim2; fps = 6)
 
 # Make sensitivity maps, normalized so SSoS = 1:
 ncoil = 2
-smap = ir_mri_sensemap_sim(dims=N, ncoil=ncoil, orbit_start=[90])
-p1 = jim(smap, "Sensitivity maps raw");
+smap_raw = ir_mri_sensemap_sim(dims=N, ncoil=ncoil, orbit_start=[90])
+p1 = jim(smap_raw, "Sensitivity maps raw");
 
-ssos = sqrt.(sum(abs.(smap).^2, dims=ndims(smap))) # SSoS
-ssos = selectdim(ssos, ndims(smap), 1)
-p2 = jim(ssos, "SSoS for ncoil=$ncoil");
+sum_last = (f, x) -> selectdim(sum(f, x; dims=ndims(x)), ndims(x), 1)
+ssos_fun = smap -> sqrt.(sum_last(abs2, smap)) # SSoS
+ssos_raw = ssos_fun(smap_raw)
+p2 = jim(ssos_raw, "SSoS for ncoil=$ncoil");
 
-for ic=1:ncoil
-    selectdim(smap, ndims(smap), ic) ./= ssos
-end
+smap = smap_raw ./ ssos_raw
 p3 = jim(smap, "Sensitivity maps");
 
-ssos = sqrt.(sum(abs.(smap).^2, dims=ndims(smap))) # SSoS
-@assert all(isapprox.(ssos,1))
+ssos = ssos_fun(smap) # SSoS
+@assert all(≈(1), ssos)
 jim(p1, p2, p3)
 
 
-# Make system matrix for dynamic non-Cartesian parallel MRI:
+#=
+## System matrix
+Make system matrix for dynamic non-Cartesian parallel MRI:
+=#
 Fs = Array{Any}(undef, nt)
-for it=1:nt # a NUFFT object for each frame
+for it in 1:nt # a NUFFT object for each frame
     Ω = [kspace[:,:,it,1][:] kspace[:,:,it,2][:]] * fov * 2pi
     Fs[it] = Anufft(Ω, N, n_shift = [N...]/2)
 end
 
 # Block diagonal system matrix, with one NUFFT per frame
-S = [Diagonal(vec(selectdim(smap, ndims(smap), ic))) for ic=1:ncoil]
+S = [Diagonal(vec(selectdim(smap, ndims(smap), ic))) for ic in 1:ncoil]
 SO = s -> LinearMapAA(s ; idim=N, odim=N) # LinearMapAO for coil maps
 AS1 = F -> vcat([F * SO(s) for s in S]...); # [A1*S1; ... ; A1*Sncoil]
 
-# Input is [N... nt], output is [nspf*Nro Ncoil nt]
+# Linear operator input is `(N..., nt)`, output is `(nspf*Nro, Ncoil nt)`
 A = block_diag([AS1(F) for F in Fs]...) # todo: refine show()
 (size(A), A._odim, A._idim)
 
 
 # Simulate k-space data via an inverse crime (todo):
+#src for it in 1:nt
+#src tmp = spectrum(objects[it]).(kspace[:,:,it,1], kspace[:,:,it,2])
 ytrue = A * xtrue
 snr2sigma = (db, yb) -> # compute noise sigma from SNR (no sqrt(2) needed)
     10^(-db/20) * norm(yb) / sqrt(length(yb))
 sig = Float32(snr2sigma(50, ytrue))
 seed!(0)
 y = ytrue + sig * randn(ComplexF32, size(ytrue))
-20*log10(norm(ytrue) / norm(y - ytrue)) # verify SNR
+ysnr = 20*log10(norm(ytrue) / norm(y - ytrue)) # verify SNR
 
 
-# Initial image via zero-fill and scaling:
-# todo: should use density compensation, perhaps via
-# [VoronoiDelaunay.jl](https://github.com/JuliaGeometry/VoronoiDelaunay.jl)
+#=
+## Initial image
+
+Initial image via zero-fill and scaling:
+todo: should use density compensation, perhaps via
+[VoronoiDelaunay.jl](https://github.com/JuliaGeometry/VoronoiDelaunay.jl)
+=#
 x0 = A' * y # zero-filled recon (for each frame)
-tmp = A * x0 # Nkspace × Ncoil × Nframe
+tmp = A * x0 # (Nkspace, Ncoil, Nframe)
 x0 = (dot(tmp,y) / norm(tmp)^2) * x0 # scale sensibly
-jimxy(x0, "initial image")
+jimxy(x0, "initial image"; nrow=2)
 
 
 # Temporal finite differences:
 Dt = diffl_map((N..., nt), length(N)+1 ; T=eltype(A))
 tmp = Dt' * (Dt * xtrue)
-jimxy(tmp, "time diff")
+jimxy(tmp, "Time differences are sparse"; nrow=2)
 
 
-# Run nonlinear CG on "temporal edge-preserving" regularized LS cost function
+#=
+## Regularized CG reconstruction
+Run nonlinear CG on "temporal edge-preserving" regularized LS cost function
+=#
 niter = 90
 delta = Float32(0.1) # small relative to temporal differences
 reg = Float32(2^20) # trial and error here
@@ -194,15 +203,15 @@ curvf = [v -> Float32(1), u -> reg]
 costs = [out[i+1][1] for i=0:niter];
 
 # Show results
-plot(
-    jimxy(xtrue, "xtrue"),
-    jimxy(xh, "recon"),
-    jimxy(xh-xtrue, "error"),
+pr = plot(
+    jimxy(xtrue, "|xtrue|"; nrow=2),
+    jimxy(xh, "|recon|"; nrow=2),
+    jimxy(xh-xtrue, "|error|"; nrow=2),
     scatter(0:niter, log.(costs), label="cost", xlabel="iteration"),
 )
 
 #
-isinteractive() && jim(:prompt, true);
+isinteractive() && prompt();
 
 # Animate true, recon, error
 anim3 = @animate for it in 1:nt
